7fecf24e48b88bc312112efca2d7a5e1de94d90b
vk <kettunen.villu@gmail.com>
2013-12-06 13:08:09 +0400

    cmake

---
+++ msvc_override.cmake
@@ -0,0 +1,19 @@
+
+
+if(MSVC)
+
+    foreach(flag_var         
+        CMAKE_CXX_FLAGS_DEBUG_INIT 
+        CMAKE_CXX_FLAGS_RELEASE_INIT
+        CMAKE_CXX_FLAGS_MINSIZEREL_INIT 
+        CMAKE_CXX_FLAGS_RELWITHDEBINFO_INIT
+        CMAKE_C_FLAGS_DEBUG_INIT 
+        CMAKE_C_FLAGS_RELEASE_INIT
+        CMAKE_C_FLAGS_MINSIZEREL_INIT 
+        CMAKE_C_FLAGS_RELWITHDEBINFO_INIT)
+        
+        string(REGEX REPLACE "/MD" "/MT" ${flag_var} "${${flag_var}}")
+        
+    endforeach(flag_var)
+
+endif(MSVC)
--- vtrc-test-server/main.cpp
+++ vtrc-test-server/main.cpp
@@ -1,4 +1,6 @@
 #include <iostream>
+#include <boost/asio.hpp>
+#include <google/protobuf/descriptor.h>
 
 int main( )
 {
