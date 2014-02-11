87918cef6c61ba5d51ac306f22d66f5646776eb3
newenclave <pikratacid@gmail.com>
2014-02-11 16:27:44 +0400

    protocol

--- vtrc-common/vtrc-exception.h
+++ vtrc-common/vtrc-exception.h
@@ -3,13 +3,21 @@
 
 #include <exception>
 
+namespace vtrc_errors {
+    class error_container;
+}
+
 namespace vtrc { namespace common {
 
     class exception: public std::exception {
+        std::string what_;
+        std::string additional_;
     public:
         exception( unsigned code, const std::string &additional );
         exception( unsigned code );
-        const char what( ) const;
+        const char *what( ) const;
+        const char *additional( ) const;
+        unsigned   code( ) const;
     };
 
 }}
