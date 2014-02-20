fabb8e235d7fe2da939ddec4ea94554bc78d2aed
newenclave <pikratacid@gmail.com>
2014-02-20 15:15:04 +0400

    protocol

---
+++ vtrc-client-base/vtrc-rpc-channel.cpp
@@ -0,0 +1,42 @@
+#include "vtrc-rpc-channel.h"
+
+namespace vtrc { namespace client {
+
+    namespace gpb = google::protobuf;
+
+    struct rpc_channel::rpc_channel_impl {
+        common::connection_iface *connection_;
+
+        rpc_channel_impl(common::connection_iface *c)
+            :connection_(c)
+        {}
+
+        void CallMethod(const gpb::MethodDescriptor* method,
+                        gpb::RpcController* controller,
+                        const gpb::Message* request,
+                        gpb::Message* response,
+                        gpb::Closure* done)
+        {
+
+        }
+    };
+
+    rpc_channel::rpc_channel( common::connection_iface *connection )
+        :impl_(new rpc_channel_impl(connection))
+    {}
+
+    rpc_channel::~rpc_channel( )
+    {
+        delete impl_;
+    }
+
+    void rpc_channel::CallMethod(const gpb::MethodDescriptor* method,
+                    gpb::RpcController* controller,
+                    const gpb::Message* request,
+                    gpb::Message* response,
+                    gpb::Closure* done)
+    {
+        impl_->CallMethod( method, controller, request, response, done );
+    }
+
+}}
---
+++ vtrc-client-base/vtrc-rpc-channel.h
@@ -0,0 +1,44 @@
+#ifndef VTRC_RCP_CHANNEL_H
+#define VTRC_RCP_CHANNEL_H
+
+#include <google/protobuf/service.h>
+#include <boost/shared_ptr.hpp>
+
+namespace google { namespace protobuf {
+    class Message;
+    class RpcController;
+    class Closure;
+    class MethodDescriptor;
+}}
+
+namespace vtrc {
+
+namespace common {
+    struct connection_iface;
+}
+
+namespace client {
+
+    class rpc_channel: public google::protobuf::RpcChannel {
+        struct rpc_channel_impl;
+        rpc_channel_impl  *impl_;
+
+    public:
+
+        rpc_channel( common::connection_iface *connection );
+        ~rpc_channel( );
+
+    private:
+
+        void CallMethod(const google::protobuf::MethodDescriptor* method,
+                        google::protobuf::RpcController* controller,
+                        const google::protobuf::Message* request,
+                        google::protobuf::Message* response,
+                        google::protobuf::Closure* done);
+    };
+
+    typedef boost::shared_ptr<rpc_channel> rpc_channel_sptr;
+
+}}
+
+#endif // VTRCRCPCHANNEL_H
