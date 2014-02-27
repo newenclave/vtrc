159c3f1629b7d2d060e4dfc12bae84eab464b657
newenclave <pikratacid@gmail.com>
2014-02-27 16:54:50 +0400

    protocol

--- vtrc-common/vtrc-exception.cpp
+++ vtrc-common/vtrc-exception.cpp
@@ -1,5 +1,65 @@
+#include <google/protobuf/descriptor.h>
+
 #include "vtrc-exception.h"
+#include "protocol/vtrc-errors.pb.h"
 
 namespace vtrc { namespace common {
+
+    namespace gpb = google::protobuf;
+
+    namespace {
+
+        std::string get_error_code_string( unsigned code, unsigned category )
+        {
+            const gpb::EnumValueDescriptor *ev =
+                vtrc_errors::errors_numbers_descriptor( )
+                                ->FindValueByNumber( code );
+
+            if( ev ) {
+                return ev->options( )
+                        .GetExtension( vtrc_errors::error_description );
+            }
+            return "Unknown error";
+        }
+    }
+
+    exception::exception( unsigned code, unsigned category )
+        :code_(code)
+        ,category_(category)
+        ,what_(get_error_code_string(code_, category_))
+    {
+
+    }
+
+    exception::exception( unsigned code )
+        :code_(code)
+        ,category_(vtrc_errors::CATEGORY_INTERNAL)
+        ,what_(get_error_code_string(code_, category_))
+    {}
+
+    exception::exception( unsigned code, const std::string &additional )
+        :code_(code)
+        ,category_(vtrc_errors::CATEGORY_INTERNAL)
+        ,what_(get_error_code_string(code_, category_))
+        ,additional_(additional)
+    {}
+
+    exception::~exception( ) throw ( )
+    {}
+
+    const char *exception::what( ) const throw ( )
+    {
+        return what_.c_str( );
+    }
+
+    const char *exception::additional( ) const
+    {
+        return additional_.c_str( );
+    }
+
+    unsigned exception::code( ) const
+    {
+        return code_;
+    }
 
 }}
--- vtrc-common/vtrc-exception.h
+++ vtrc-common/vtrc-exception.h
@@ -11,13 +11,20 @@ namespace vtrc_errors {
 namespace vtrc { namespace common {
 
     class exception: public std::exception {
+
+        unsigned    code_;
+        unsigned    category_;
+
         std::string what_;
         std::string additional_;
-    public:
-        exception( unsigned code, const std::string &additional );
-        exception( unsigned code );
 
-        ~exception( ) throw ( );
+    public:
+
+        explicit exception( unsigned code, unsigned category );
+        exception( unsigned code );
+        exception( unsigned code, const std::string &additional );
+
+        virtual ~exception( ) throw ( );
 
         const char *what( ) const throw ( );
         const char *additional( ) const;
