91ca996139c1c5ff84b3c9bd82ca386fe5d7d108
newenclave <pikratacid@gmail.com>
2014-03-01 16:55:35 +0400

    protocol

--- vtrc-common/proto-helper/message-utilities.h
+++ vtrc-common/proto-helper/message-utilities.h
@@ -1,6 +1,19 @@
 #ifndef MESSAGE_UTILITIES_H
 #define MESSAGE_UTILITIES_H
 
+namespace google { namespace protobuf {
+    class Message;
+}}
 
+namespace vtrc { namespace utilities {
+
+    bool make_message_diff( google::protobuf::Message const &templ,
+                            google::protobuf::Message const &src,
+                            google::protobuf::Message &result  );
+
+    void merge_messages( google::protobuf::Message &to,
+                         google::protobuf::Message const &from );
+
+}}
 
 #endif // MESSAGEUTILITIES_H
