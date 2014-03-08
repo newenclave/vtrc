3dd2fdcee69e936e524b6371017411fa99474e88
newenclave <pikratacid@gmail.com>
2014-03-08 14:59:24 +0400

    protocol

--- vtrc-common/vtrc-transformer-erseefor.cpp
+++ vtrc-common/vtrc-transformer-erseefor.cpp
@@ -15,7 +15,7 @@ namespace vtrc { namespace common {
             transformer_erseefor( const char *transform_key, size_t t_length,
                                   const char *revert_key,    size_t r_length )
                 :crypt_((const unsigned char *)transform_key, t_length)
-                ,uncrypt_((const unsigned char *)revert_key, t_length)
+                ,uncrypt_((const unsigned char *)revert_key,  r_length)
             {
                 crypt_.drop( drop_key_init );
                 uncrypt_.drop( drop_key_init );
