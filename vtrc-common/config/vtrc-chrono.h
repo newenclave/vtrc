fe31f76494601e20a6d3680f62f5f492b71945fb
newenclave <pikratacid@gmail.com>
2014-03-10 14:10:40 +0400

    ++

---
+++ vtrc-common/config/vtrc-chrono.h
@@ -0,0 +1,12 @@
+#ifndef VTRC_CHRONO_H
+#define VTRC_CHRONO_H
+
+#include <boost/chrono.hpp>
+
+namespace vtrc {
+    namespace chrono {
+        using namespace boost::chrono;
+    }
+}
+
+#endif // VTRCCHRONO_H
