dd24a88bb685644045011dfbee206653cccb83b6
newenclave <pikratacid@gmail.com>
2014-03-26 18:07:22 +0400

    protocol

--- vtrc-test-server/main.cpp
+++ vtrc-test-server/main.cpp
@@ -73,7 +73,7 @@ void test_send( common::connection_iface *connection )
             get_call_context( )->get_lowlevel_message( );
 
     vtrc_rpc_lowlevel::lowlevel_unit llu;
-    connection->get_protocol( ).send_message( llu );
+    //connection->get_protocol( ).send_message( llu );
 
     vtrc_service::internal::Stub ping( ev.get( ));
     vtrc_service::ping_req preq;
