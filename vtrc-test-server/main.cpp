ffb06f16a255992a929b9c1f4e4a252fdb9d8e77
newenclave <pikratacid@gmail.com>
2014-03-31 18:53:08 +0400

    done handlers

--- vtrc-test-server/main.cpp
+++ vtrc-test-server/main.cpp
@@ -246,7 +246,7 @@ int main( ) try {
     tcp4_ep->start( );
     tcp6_ep->start( );
 
-    boost::this_thread::sleep_for( vtrc::chrono::milliseconds(120009999) );
+    boost::this_thread::sleep_for( vtrc::chrono::milliseconds(12000) );
 
     std::cout << "Stoppped. Wait ... \n";
 
