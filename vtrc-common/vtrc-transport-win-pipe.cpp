469ae2c69a41b0526f23098bec3593edd3cf3768
vk <kettunen.villu@gmail.com>
2014-04-01 12:44:03 +0400

    add win pipe

---
+++ vtrc-common/vtrc-transport-win-pipe.cpp
@@ -0,0 +1,93 @@
+
+#include "vtrc-transport-win-pipe.h"
+
+#if 0
+#ifdef _WIN32
+
+#include "vtrc-transport-stream-impl.h"
+
+namespace vtrc { namespace common {
+
+    namespace basio = boost::asio;
+    namespace bip   = boost::asio::ip;
+    namespace bsys  = boost::system;
+
+    namespace {
+        typedef transport_win_pipe::socket_type socket_type;
+        typedef transport_impl<socket_type, transport_win_pipe> impl_type;
+    }
+
+    struct transport_win_pipe::impl: public impl_type {
+
+        impl( vtrc::shared_ptr<socket_type> s, const std::string &n )
+            :impl_type(s, n)
+        {  }
+
+        std::string prepare_for_write(const char *data, size_t len)
+        {
+            return get_parent( )->prepare_for_write( data, len );
+        }
+    };
+
+    transport_win_pipe::transport_win_pipe( vtrc::shared_ptr<socket_type> sock )
+        :impl_(new impl(sock, "tcp"))
+    {
+        impl_->set_parent( this );
+    }
+
+    transport_win_pipe::~transport_win_pipe(  )
+    {
+        delete impl_;
+    }
+
+    const char *transport_win_pipe::name( ) const
+    {
+        return impl_->name( );
+    }
+
+    void transport_win_pipe::close( )
+    {
+        impl_->close( );
+    }
+
+    bool transport_win_pipe::active( ) const
+    {
+        return impl_->active( );
+    }
+
+    common::enviroment &transport_win_pipe::get_enviroment( )
+    {
+        return impl_->get_enviroment( );
+    }
+
+    void transport_win_pipe::write( const char *data, size_t length )
+    {
+        return impl_->write( data, length );
+    }
+
+    void transport_win_pipe::write(const char *data, size_t length,
+                              closure_type &success)
+    {
+        impl_->write( data, length, success );
+    }
+
+    std::string transport_win_pipe::prepare_for_write(const char *data, size_t len)
+    {
+        return impl_->prepare_for_write( data, len );
+    }
+
+    socket_type &transport_win_pipe::get_socket( )
+    {
+        return impl_->get_socket( );
+    }
+
+    const socket_type &transport_win_pipe::get_socket( ) const
+    {
+        return impl_->get_socket( );
+    }
+
+}}
+
+
+#endif
+#endif
---
+++ vtrc-common/vtrc-transport-win-pipe.h
@@ -0,0 +1,59 @@
+#include "vtrc-transport-iface.h"
+
+#ifdef _WIN32
+
+#include <boost/asio/windows/stream_handle.hpp>
+
+#include "vtrc-memory.h"
+
+namespace boost {
+
+namespace asio {
+    class    io_service;
+}
+
+namespace system {
+    class error_code;
+}}
+
+namespace vtrc { namespace common {
+
+    class transport_win_pipe: public transport_iface {
+
+        struct impl;
+        friend struct impl;
+        impl *impl_;
+
+        transport_win_pipe( const transport_win_pipe & );
+        transport_win_pipe& operator = ( const transport_win_pipe & );
+
+    public:
+
+        typedef boost::asio::windows::stream_handle socket_type;
+
+        transport_win_pipe( vtrc::shared_ptr<socket_type> sock );
+        virtual ~transport_win_pipe(  );
+
+        const char *name( ) const                     ;
+        void close( )                                 ;
+        bool active( ) const                          ;
+
+        common::enviroment      &get_enviroment( )    ;
+
+        socket_type             &get_socket( )        ;
+        const socket_type       &get_socket( ) const  ;
+
+        void write( const char *data, size_t length ) ;
+        void write( const char *data, size_t length, closure_type &success ) ;
+
+        virtual void on_write_error( const boost::system::error_code &err ) = 0;
+
+    private:
+
+        virtual std::string prepare_for_write( const char *data, size_t len );
+    };
+
+}}
+
+
+#endif
