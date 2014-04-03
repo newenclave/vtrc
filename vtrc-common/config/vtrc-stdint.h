66ef428bee0b7ac5d6ec96d939f398e9c797e273
newenclave <pikratacid@gmail.com>
2014-04-03 11:38:47 +0400

    transform

--- vtrc-common/config/vtrc-stdint.h
+++ vtrc-common/config/vtrc-stdint.h
@@ -10,13 +10,13 @@
 
 #else
 
-typedef signed      char int8_t;
+typedef signed      char  int8_t;
 typedef unsigned    char uint8_t;
-typedef signed     short int16_t;
+typedef signed     short  int16_t;
 typedef unsigned   short uint16_t;
-typedef signed   __int32 int32_t;
+typedef signed   __int32  int32_t;
 typedef unsigned __int32 uint32_t;
-typedef signed   __int64 int64_t;
+typedef signed   __int64  int64_t;
 typedef unsigned __int64 uint64_t;
 
 #endif
