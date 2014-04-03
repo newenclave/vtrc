2f83d0302b9e7265ab4d99780074ec7e30d51df8
newenclave <pikratacid@gmail.com>
2014-04-03 10:30:07 +0400

    stdint

---
+++ vtrc-common/config/vtrc-stdint.h
@@ -0,0 +1,30 @@
+#ifndef VTRC_STDINT_H
+#define VTRC_STDINT_H
+
+#ifdef _WIN32
+
+#if _MSC_VER >= 1600
+
+// Visual Studio 2010+
+#include <stdint.h>
+
+#else
+
+typedef signed      char int8_t;
+typedef unsigned    char uint8_t;
+typedef signed     short int16_t;
+typedef unsigned   short uint16_t;
+typedef signed   __int32 int32_t;
+typedef unsigned __int32 uint32_t;
+typedef signed   __int64 int64_t;
+typedef unsigned __int64 uint64_t;
+
+#endif
+
+#else
+
+#include <stdint.h>
+
+#endif
+
+#endif // VTRCSTDINT_H
