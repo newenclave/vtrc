041f1eedd06b5f405797a1fa9dc8b49cd19805a6
newenclave <pikratacid@gmail.com>
2014-04-07 13:40:09 +0400

    proto init

--- vtrc-common/vtrc-transport-win-pipe.cpp
+++ vtrc-common/vtrc-transport-win-pipe.cpp
@@ -63,7 +63,7 @@ namespace vtrc { namespace common {
     }
 
     void transport_win_pipe::write(const char *data, size_t length,
-                     const closure_type &success, bool  on_send_success)
+                     const closure_type &success, bool on_send_success)
     {
         impl_->write( data, length, success, on_send_success );
     }
