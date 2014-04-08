823e5bdc2dea84e448f9ece8d2f20d8def23aebf
newenclave <pikratacid@gmail.com>
2014-04-08 07:35:36 +0400

    proto

---
+++ vtrc-server/vtrc-endpoints.cpp
@@ -0,0 +1,13 @@
+
+#include "vtrc-endpoint-iface.h"
+
+namespace vtrc { namespace server { namespace endpoints {
+
+    endpoint_options default_options( )
+    {
+        endpoint_options def_opts = { 5, 1024 * 1024, 20, 4096 };
+        return def_opts;
+    }
+
+}}}
+
