7379ee7df63bf66d5738256596ccfb0bd067fbfb
V.K <kettunen.villu@gmail.com>
2014-04-08 23:20:53 +0400

    protocol

--- vtrc-test-server/main.cpp
+++ vtrc-test-server/main.cpp
@@ -279,7 +279,7 @@ int main( ) try {
     tcp4_ep->start( );
     tcp6_ep->start( );
 
-    boost::this_thread::sleep_for( vtrc::chrono::seconds(10 ) );
+    boost::this_thread::sleep_for( vtrc::chrono::seconds( 10 ) );
 
     std::cout << "Stoppped. Wait ... \n";
 
