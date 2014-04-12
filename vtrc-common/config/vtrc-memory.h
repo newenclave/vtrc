dd8625cf57890a12e413849dfadd712c9080777d
newenclave <pikratacid@gmail.com>
2014-04-12 12:39:03 +0400

    memory

--- vtrc-common/config/vtrc-memory.h
+++ vtrc-common/config/vtrc-memory.h
@@ -4,7 +4,6 @@
 #include "boost/shared_ptr.hpp"
 #include "boost/weak_ptr.hpp"
 #include "boost/make_shared.hpp"
-#include "boost/scoped_ptr.hpp"
 #include "boost/enable_shared_from_this.hpp"
 
 namespace vtrc {
