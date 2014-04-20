4e84e22982df5162d07755acd55f8d8d09a0db9f
newenclave <pikratacid@gmail.com>
2014-04-20 13:55:29 +0400

    example

---
+++ examples/calculator/client/main.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+
+#include "protocol/calculator.pb.h"
+#include "protocol/vtrc-errors.pb.h"
+
+int main( )
+{
+    return 0;
+}
---
+++ examples/calculator/server/main.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include "protocol/calculator.pb.h"
+#include "protocol/vtrc-errors.pb.h"
+
+
+int main( )
+{
+    return 0;
+}
+
