37fb9e0d8d44b77d5484fd7087af930a749730cd
newenclave <pikratacid@gmail.com>
2014-05-03 14:16:57 +0400

    fs examples

--- examples/remote-fs/server/main.cpp
+++ examples/remote-fs/server/main.cpp
@@ -1,8 +1,24 @@
 #include <iostream>
-#include <boost/program_options.hpp>
+#include "boost/program_options.hpp"
+#include "vtrc-server/vtrc-application.h"
+#include "vtrc-common/vtrc-pool-pair.h"
+
+using namespace vtrc;
+
+class fs_application: public server::application {
+public:
+    fs_application( common::pool_pair &pp )
+        :server::application(pp)
+    { }
+};
 
 int main( int argc, char *argv[] )
 {
+    common::pool_pair pp( 1, 1 );
 
+    fs_application app( pp );
+
+    pp.stop_all( );
+    pp.join_all( );
     return 0;
 }
