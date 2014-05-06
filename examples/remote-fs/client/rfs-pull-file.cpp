9aaf71ee811da8ef9678763f7468c300a623828d
newenclave <pikratacid@gmail.com>
2014-05-06 12:40:57 +0400

    examples

---
+++ examples/remote-fs/client/rfs-pull-file.cpp
@@ -0,0 +1,69 @@
+
+#include <iostream>
+#include <string>
+#include <fstream>
+
+#include "vtrc-client-base/vtrc-client.h"
+#include "vtrc-common/vtrc-exception.h"
+
+#include "remote-fs-iface.h"
+
+#include "vtrc-memory.h"
+
+namespace rfs_examples {
+
+    using namespace vtrc;
+
+    namespace {
+        std::string leaf_of( const std::string &path )
+        {
+            std::string::const_reverse_iterator b(path.rbegin( ));
+            std::string::const_reverse_iterator e(path.rend( ));
+            for( ; b!=e ;++b ) {
+                if( *b == '/' || *b == '\\' ) {
+                    return std::string( b.base( ), path.end( ) );
+                }
+            }
+            return path;
+       }
+    }
+
+    void pull_file( client::vtrc_client_sptr client,
+                    vtrc::shared_ptr<interfaces::remote_fs> &impl,
+                    const std::string &remote_path, size_t block_size )
+    {
+        size_t remote_size = -1;
+        try {
+            remote_size = impl->file_size( remote_path );
+            std::cout << "Remote file size is: " << remote_size << "\n";
+        } catch( const vtrc::common::exception &ex ) {
+            std::cout << "Remote file size is unknown: "
+                      << remote_path
+                      << " '" << ex.what( ) << "; " << ex.additional( ) << "'"
+                      << "\n";
+        }
+
+        std::string name = leaf_of( remote_path );
+        vtrc::shared_ptr<interfaces::remote_file> rem_f
+                ( interfaces::create_remote_file( client, remote_path, "rb" ) );
+
+        std::cout << "Open remote file success.\n"
+                  << "Starting...\n"
+                  << "Block size = " << block_size
+                  << std::endl;
+
+        std::ofstream f;
+        f.open(name.c_str( ), std::ofstream::out );
+
+        std::string block;
+        size_t total = 0;
+
+        while( rem_f->read( block, block_size ) ) {
+            total += block.size( );
+            std::cout << "Got " << total << " bytes\r";
+            f.write( block.c_str( ), block.size( ) );
+        }
+        std::cout << "\nDownload complete\n";
+    }
+
+}
---
+++ examples/remote-fs/client/rfs-push-file.cpp
@@ -0,0 +1,67 @@
+
+#include <iostream>
+#include <string>
+#include <fstream>
+
+#include "vtrc-client-base/vtrc-client.h"
+#include "vtrc-common/vtrc-exception.h"
+
+#include "remote-fs-iface.h"
+
+#include "vtrc-memory.h"
+
+
+namespace rfs_examples {
+
+    using namespace vtrc;
+
+    namespace {
+        std::string leaf_of( const std::string &path )
+        {
+            std::string::const_reverse_iterator b(path.rbegin( ));
+            std::string::const_reverse_iterator e(path.rend( ));
+            for( ; b!=e ;++b ) {
+                if( *b == '/' || *b == '\\' ) {
+                    return std::string( b.base( ), path.end( ) );
+                }
+            }
+            return path;
+       }
+    }
+
+    void push_file( client::vtrc_client_sptr client,
+                    const std::string &local_path, size_t block_size )
+    {
+        std::string name = leaf_of( local_path );
+        vtrc::shared_ptr<interfaces::remote_file> rem_f
+                ( interfaces::create_remote_file( client, name, "wb" ) );
+
+        std::cout << "Open remote file success.\n"
+                  << "Starting...\n"
+                  << "Block size = " << block_size
+                  << std::endl;
+
+        std::ifstream f;
+        f.open(local_path.c_str( ), std::ofstream::in );
+
+        std::string block(block_size, 0);
+        size_t total = 0;
+
+        size_t r = f.readsome( &block[0], block_size );
+        while( r ) {
+            size_t shift = 0;
+            while ( r ) {
+                size_t w = rem_f->write( block.c_str( ) + shift, r );
+                total += w;
+                shift += w;
+                r -= w;
+                std::cout << "Post " << total << " bytes\r";
+            }
+            r = f.readsome( &block[0], block_size );
+        }
+
+        std::cout << "\nUpload complete\n";
+
+    }
+
+}
