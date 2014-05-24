0e71570984596e6145966b17fcac22569f0b8aa5
V.K <kettunen.villu@gmail.com>
2014-05-24 21:33:38 +0400

    examples

--- examples/stress/server/stress-service-impl.cpp
+++ examples/stress/server/stress-service-impl.cpp
@@ -1,5 +1,32 @@
+#include "stress-service-impl.h"
+#include "protocol/stress.pb.h"
+#include "vtrc-common/vtrc-connection-iface.h"
 
+namespace gpb = google::protobuf;
+using namespace vtrc;
+
+namespace  {
+
+    class stress_service_impl: public vtrc_example::stress_service {
+        common::connection_iface *c_;
+
+    public:
+
+        stress_service_impl( common::connection_iface *c )
+            :c_(c)
+        { }
+
+    private:
+
+    };
+
+}
 
 namespace stress {
+
+    gpb::Service *create_service( common::connection_iface *c )
+    {
+        return new stress_service_impl( c );
+    }
 
 }
--- examples/stress/server/stress-service-impl.h
+++ examples/stress/server/stress-service-impl.h
@@ -1,8 +1,17 @@
 #ifndef STRESS_SERVICE_IMPL_H
 #define STRESS_SERVICE_IMPL_H
 
-namespace stress {
+namespace google { namespace protobuf {
+    class Service;
+}}
 
+namespace vtrc { namespace common {
+    struct connection_iface;
+}}
+
+namespace stress {
+    google::protobuf::Service *create_service(
+                                            vtrc::common::connection_iface *c );
 }
 
 #endif // STRESS_SERVICE_IMPL_H
