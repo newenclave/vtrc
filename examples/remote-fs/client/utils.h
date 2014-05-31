ebab414fe3071891bb95ca88176ae0664288c199
newenclave <pikratacid@gmail.com>
2014-05-31 12:28:51 +0400

    proto

--- examples/remote-fs/client/utils.h
+++ examples/remote-fs/client/utils.h
@@ -6,10 +6,5 @@ namespace rfs_examples {
     std::string leaf_of( const std::string &path );
     std::string percents_string( double value, double maximum );
 
-#ifdef _WIN32
-    std::wstring make_ws_string_from_utf8( const std::string &src );
-    std::wstring make_utf8_string( const std::wstring &src );
-#endif
-
 }
 
