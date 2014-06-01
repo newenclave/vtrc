1bc943adcdd4886ad95bc20c00d38ae7bab60b42
newenclave <pikratacid@gmail.com>
2014-06-01 13:33:23 +0400

    signals/slots

--- vtrc-common/vtrc-signal-declaration.h
+++ vtrc-common/vtrc-signal-declaration.h
@@ -20,8 +20,11 @@
         typedef BOOST_PP_CAT( name, _type )::group_type                        \
                 BOOST_PP_CAT( name, _group_type );                             \
         template <typename SlotType>                                           \
-        boost::signals2::connection BOOST_PP_CAT(name, _connect)( SlotType s ) \
-        { return BOOST_PP_CAT( name, _ ).connect( s ); }                       \
+        boost::signals2::connection                                            \
+        BOOST_PP_CAT(name, _connect )                                          \
+             ( SlotType s,                                                     \
+             boost::signals2::connect_position pos = boost::signals2::at_back )\
+        { return BOOST_PP_CAT( name, _ ).connect( s, pos ); }                  \
         void BOOST_PP_CAT( name, _disconnect_all_slots )( )                    \
         { BOOST_PP_CAT( name, _ ).disconnect_all_slots( ); }                   \
     protected:                                                                 \
@@ -45,8 +48,11 @@
         typedef BOOST_PP_CAT( name, _type )::group_type                        \
                 BOOST_PP_CAT( name, _group_type );                             \
         template <typename SlotType>                                           \
-        boost::signals2::connection BOOST_PP_CAT(name, _connect)( SlotType s ) \
-        { return BOOST_PP_CAT( name, _ ).connect( s ); }                       \
+        boost::signals2::connection                                            \
+        BOOST_PP_CAT(name, _connect )                                          \
+             ( SlotType s,                                                     \
+             boost::signals2::connect_position pos = boost::signals2::at_back )\
+        { return BOOST_PP_CAT( name, _ ).connect( s, pos ); }                  \
         void BOOST_PP_CAT( name, _disconnect_all_slots )( )                    \
         { BOOST_PP_CAT( name, _ ).disconnect_all_slots( ); }                   \
     protected:                                                                 \
@@ -56,7 +62,6 @@
         BOOST_PP_CAT( name, _type ) BOOST_PP_CAT( name, _ )
 
 #define VTRC_DECLARE_SIGNAL VTRC_DECLARE_SIGNAL_SAFE
-
 
 /*
 
@@ -73,13 +78,26 @@ class test {
                 void( ),
                 boost::signals2::keywords::mutex_type<boost::mutex>
               >::type on_event_type;
+
         typedef on_event_type::slot_type on_event_slot_type;    // slot_type
+
         typedef on_event_type::group_type on_event_group_type;  // group_type
+
+        /// connect to signal
         template <typename SlotType>
-        boost::signals2::connection  on_event_connect( SlotType s )
-        { return on_event_.connect( s ); }
+        boost::signals2::connection on_event_connect
+            ( SlotType s,
+              boost::signals2::connect_position pos = boost::signals2::at_back)
+        {
+            return on_event_.connect( s, pos );
+        }
+
+        /// disconnect all slots
         void on_event_disconnect_all_slots( )
-        { on_event_.disconnect_all_slots( ); }
+        {
+            on_event_.disconnect_all_slots( );
+        }
+
     protected:
         on_events_type &get_on_event( ) { return on_event_; }
     private:
