e7a35dc1173f4d2c2da102002f0717e70d5c91e9
newenclave <pikratacid@gmail.com>
2014-06-04 11:58:36 +0400

    signals

--- vtrc-common/vtrc-signal-declaration.h
+++ vtrc-common/vtrc-signal-declaration.h
@@ -8,57 +8,41 @@
 
 #include "vtrc-mutex.h"
 
-// ====== SAFE
-#define VTRC_DECLARE_SIGNAL_SAFE( name, sig_type )                             \
+#define VTRC_DECLARE_SIGNAL_COMMON( Access, Name, SigType, MutexType )         \
     public:                                                                    \
-        typedef boost::signals2::signal_type<                                  \
-            sig_type,                                                          \
-            boost::signals2::keywords::mutex_type<vtrc::mutex>                 \
-        >::type BOOST_PP_CAT( name, _type );                                   \
-        typedef BOOST_PP_CAT( name, _type )::slot_type                         \
-                BOOST_PP_CAT( name, _slot_type );                              \
-        typedef BOOST_PP_CAT( name, _type )::group_type                        \
-                BOOST_PP_CAT( name, _group_type );                             \
+        typedef boost::signals2::signal_type <                                 \
+            SigType,                                                           \
+            boost::signals2::keywords::mutex_type<MutexType>                   \
+        >::type BOOST_PP_CAT( Name, _type );                                   \
+        typedef BOOST_PP_CAT( Name, _type )::slot_type                         \
+                BOOST_PP_CAT( Name, _slot_type );                              \
+        typedef BOOST_PP_CAT( Name, _type )::group_type                        \
+                BOOST_PP_CAT( Name, _group_type );                             \
         template <typename SlotType>                                           \
         boost::signals2::connection                                            \
-        BOOST_PP_CAT(name, _connect )                                          \
+        BOOST_PP_CAT( Name, _connect )                                         \
              ( SlotType s,                                                     \
              boost::signals2::connect_position pos = boost::signals2::at_back )\
-        { return BOOST_PP_CAT( name, _ ).connect( s, pos ); }                  \
-        void BOOST_PP_CAT( name, _disconnect_all_slots )( )                    \
-        { BOOST_PP_CAT( name, _ ).disconnect_all_slots( ); }                   \
-    protected:                                                                 \
-        BOOST_PP_CAT( name, _type )&  BOOST_PP_CAT( get_, name )( )            \
-        { return BOOST_PP_CAT( name, _ ); }                                    \
+        { return BOOST_PP_CAT( Name, _ ).connect( s, pos ); }                  \
+        void BOOST_PP_CAT( Name, _disconnect_all_slots )( )                    \
+        { BOOST_PP_CAT( Name, _ ).disconnect_all_slots( ); }                   \
+    Access:                                                                    \
+        BOOST_PP_CAT( Name, _type )&  BOOST_PP_CAT( get_, Name )( )            \
+        { return BOOST_PP_CAT( Name, _ ); }                                    \
     private:                                                                   \
-        BOOST_PP_CAT( name, _type ) BOOST_PP_CAT( name, _ )
+        BOOST_PP_CAT( Name, _type ) BOOST_PP_CAT( Name, _ )
+
+
+// ====== SAFE
+
+#define VTRC_DECLARE_SIGNAL_SAFE( Name, SigType )               \
+        VTRC_DECLARE_SIGNAL_COMMON( protected, Name, SigType,   \
+                                    vtrc::mutex )
 
 // ====== UNSAFE
-#define VTRC_DECLARE_SIGNAL_UNSAFE( name, sig_type )                           \
-    public:                                                                    \
-        typedef boost::signals2::signal_type<                                  \
-            sig_type,                                                          \
-            boost::signals2::keywords::mutex_type <                            \
-                  boost::signals2::dummy_mutex                                 \
-            >                                                                  \
-        >::type BOOST_PP_CAT( name, _type );                                   \
-        typedef BOOST_PP_CAT( name, _type )::slot_type                         \
-                BOOST_PP_CAT( name, _slot_type );                              \
-        typedef BOOST_PP_CAT( name, _type )::group_type                        \
-                BOOST_PP_CAT( name, _group_type );                             \
-        template <typename SlotType>                                           \
-        boost::signals2::connection                                            \
-        BOOST_PP_CAT(name, _connect )                                          \
-             ( SlotType s,                                                     \
-             boost::signals2::connect_position pos = boost::signals2::at_back )\
-        { return BOOST_PP_CAT( name, _ ).connect( s, pos ); }                  \
-        void BOOST_PP_CAT( name, _disconnect_all_slots )( )                    \
-        { BOOST_PP_CAT( name, _ ).disconnect_all_slots( ); }                   \
-    protected:                                                                 \
-        BOOST_PP_CAT( name, _type )&  BOOST_PP_CAT( get_, name )( )            \
-        { return BOOST_PP_CAT( name, _ ); }                                    \
-    private:                                                                   \
-        BOOST_PP_CAT( name, _type ) BOOST_PP_CAT( name, _ )
+#define VTRC_DECLARE_SIGNAL_UNSAFE( Name, SigType )             \
+        VTRC_DECLARE_SIGNAL_COMMON( protected, Name, SigType,   \
+                                   boost::signals2::dummy_mutex)
 
 #define VTRC_DECLARE_SIGNAL VTRC_DECLARE_SIGNAL_SAFE
 
