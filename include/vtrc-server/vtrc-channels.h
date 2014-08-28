9131b0861e32788c62c149fc19225df6a9351daf
newenclave <pikratacid@gmail.com>
2014-08-28 13:50:34 +0400

    proto

--- include/vtrc-server/vtrc-channels.h
+++ include/vtrc-server/vtrc-channels.h
@@ -5,40 +5,36 @@
 #include "vtrc-common/vtrc-connection-iface.h"
 #include "vtrc-common/vtrc-connection-list.h"
 
-namespace vtrc { namespace server {
+namespace vtrc { namespace server { namespace channels {
 
-namespace channels {
+    namespace unicast {
 
-namespace unicast {
+        /// do not use client's call context
+        common::rpc_channel
+            *create_event_channel( common::connection_iface_sptr c,
+                                              bool disable_wait = true );
 
-    /// do not use client's call context
-    common::rpc_channel
-        *create_event_channel( common::connection_iface_sptr c,
-                                          bool disable_wait = true );
+        /// use client's call context
+        common::rpc_channel
+            *create_callback_channel( common::connection_iface_sptr c,
+                                              bool disable_wait = false );
 
-    /// use client's call context
-    common::rpc_channel
-        *create_callback_channel( common::connection_iface_sptr c,
-                                          bool disable_wait = false );
+        common::rpc_channel *create( common::connection_iface_sptr c,
+                            unsigned flags = common::rpc_channel::DEFAULT );
 
-    common::rpc_channel *create( common::connection_iface_sptr c,
-                        unsigned flags = common::rpc_channel::DEFAULT );
+    } // unicast
 
-} // unicast
+    namespace broadcast {
 
-namespace broadcast {
+        common::rpc_channel *create_event_channel(
+                                vtrc::shared_ptr<common::connection_list> cl,
+                                common::connection_iface_sptr c );
 
-    common::rpc_channel *create_event_channel(
-                            vtrc::shared_ptr<common::connection_list> cl,
-                            common::connection_iface_sptr c );
+        common::rpc_channel *create_event_channel(
+                                vtrc::shared_ptr<common::connection_list> cl );
 
-    common::rpc_channel *create_event_channel(
-                            vtrc::shared_ptr<common::connection_list> cl );
+    } // broadcast
 
-} // broadcast
-
-} // channels
-
-}}
+}}}
 
 #endif // VTRC_UNICAST_CHANNELS_H
