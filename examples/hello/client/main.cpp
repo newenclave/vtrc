d547d3118913ef219dee2488d56d0d3434e0ee67
newenclave <pikratacid@gmail.com>
2014-08-28 16:42:21 +0400

    proto

---
+++ examples/hello/client/main.cpp
@@ -0,0 +1,71 @@
+#include <iostream>
+
+#include "vtrc-client-base/vtrc-client.h"
+#include "vtrc-common/vtrc-thread-pool.h"
+#include "vtrc-common/vtrc-stub-wrapper.h"
+
+#include "protocol/hello.pb.h"
+
+#include "boost/lexical_cast.hpp"
+
+using namespace vtrc;
+
+namespace {
+    void on_connect( )
+    {
+        std::cout << "connect...";
+    }
+    void on_ready( )
+    {
+        std::cout << "ready...";
+    }
+    void on_disconnect( )
+    {
+        std::cout << "disconnect...";
+    }
+}
+
+int main( int argc, const char **argv )
+{
+    common::thread_pool tp( 1 );
+
+    unsigned short port = 56560;
+
+    if( argc > 1 ) {
+        port = boost::lexical_cast<unsigned short>(argv[1]);
+    }
+
+    client::vtrc_client_sptr cl(
+                client::vtrc_client::create( tp.get_io_service( ) ) );
+
+
+    cl->on_connect_connect ( on_connect );
+    cl->on_ready_connect ( on_ready );
+    cl->on_disconnect_connect( on_disconnect );
+
+    std::cout <<  "Connecting..." << std::endl;
+
+    cl->connect( "127.0.0.1", port );
+
+    std::cout << "Ok" << std::endl;
+
+    vtrc::unique_ptr<common::rpc_channel> channel(cl->create_channel( ));
+
+    common::stub_wrapper<howto::hello_service_Stub> hello(channel.get( ));
+
+    howto::request_message  req;
+    howto::response_message res;
+
+    req.set_name( "%USERNAME%" );
+
+    typedef howto::hello_service_Stub stub_type;
+
+    hello.call( &stub_type::send_hello, &req, &res );
+
+    std::cout <<  res.hello( ) << std::endl;
+
+    tp.stop( );
+    tp.join_all( );
+
+    return 0;
+}
---
+++ examples/hello/server/main.cpp
@@ -0,0 +1,81 @@
+#include <iostream>
+
+#include "vtrc-server/vtrc-application.h"
+#include "vtrc-server/vtrc-listener-tcp.h"
+#include "vtrc-server/vtrc-listener-local.h"
+
+#include "vtrc-common/vtrc-pool-pair.h"
+#include "vtrc-common/vtrc-rpc-service-wrapper.h"
+
+#include "protocol/hello.pb.h" /// hello protocol
+
+#include "boost/lexical_cast.hpp"
+
+using namespace vtrc;
+
+namespace {
+
+class  hello_service_impl: public howto::hello_service {
+
+    void send_hello(::google::protobuf::RpcController* controller,
+                    const ::howto::request_message* request,
+                    ::howto::response_message* response,
+                    ::google::protobuf::Closure* done) /*override*/
+    {
+        std::ostringstream oss;
+
+        oss << "Hello " << request->name( )
+            << " from hello_service_impl::send_hello!";
+
+        response->set_hello( oss.str( ) );
+        done->Run( );
+    }
+};
+
+class hello_application: public server::application {
+
+public:
+    hello_application( vtrc::common::pool_pair &pp )
+        :vtrc::server::application(pp)
+    { }
+
+    vtrc::shared_ptr<common::rpc_service_wrapper>
+           get_service_by_name( common::connection_iface* connection,
+                                     const std::string &service_name )
+    {
+        if( service_name == hello_service_impl::descriptor( )->full_name( ) ) {
+
+             hello_service_impl *new_impl = new  hello_service_impl;
+
+             return vtrc::make_shared<common::rpc_service_wrapper>( new_impl );
+
+        }
+        return vtrc::shared_ptr<common::rpc_service_wrapper>( );
+    }
+
+};
+
+}
+
+int main( int argc, const char **argv )
+{
+    common::pool_pair pp( 1, 1 );
+
+    hello_application app( pp );
+
+    unsigned short port = 56560;
+
+    if( argc > 1 ) {
+        port = boost::lexical_cast<unsigned short>(argv[1]);
+    }
+
+    vtrc::shared_ptr<server::listener>
+            tcp( server::listeners::tcp::create( app, "0.0.0.0", port ) );
+
+    tcp->start( );
+
+    pp.join_all( );
+
+    return 0;
+}
+
