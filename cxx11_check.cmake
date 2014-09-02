86616ff33dceff01b946a053d79e5e8a229bc252
V.K <kettunen.villu@gmail.com>
2014-09-02 21:31:07 +0400

    cmake

---
+++ cxx11_check.cmake
@@ -0,0 +1,27 @@
+macro( check_cxx11 VAR_ENABLED )
+
+#-std=c++11 by default
+
+set( ${VAR_ENABLED} 1 )
+
+if( MSVC )                                          ### MS
+
+    if( NOT ( MSVC10 OR MSVC11 OR MSVC12 ) )
+        set( ${VAR_ENABLED} 0 )
+    endif( )
+
+elseif( CMAKE_COMPILER_IS_GNUCXX )                  ### CNU
+
+    if( ${CMAKE_CXX_COMPILER_VERSION} VERSION_LESS 4.6 )
+        set( ${VAR_ENABLED} 0 )
+    endif( )
+
+elseif( CMAKE_CXX_COMPILER_ID STREQUAL "Clang" )    ### Clang
+
+    if( ${CMAKE_CXX_COMPILER_VERSION} VERSION_LESS 3.1 )
+        set( ${VAR_ENABLED} 0 )
+    endif( )
+
+endif( )
+
+endmacro( )
--- cxx11_detect.cmake
+++
@@ -1,3 +0,0 @@
-macro( check_cxx11 VAR_ENABLED VAR_COMMAND )
-
-endmacro( )
