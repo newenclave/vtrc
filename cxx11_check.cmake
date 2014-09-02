ae6c8936ada62719d481a280478fc3a357fad5b4
V.K <kettunen.villu@gmail.com>
2014-09-02 21:38:35 +0400

    cmake

--- cxx11_check.cmake
+++ cxx11_check.cmake
@@ -4,24 +4,24 @@ macro( check_cxx11 VAR_ENABLED )
 
 set( ${VAR_ENABLED} 1 )
 
-if( MSVC )                                          ### MS
+    if( MSVC )                                          ### MS
 
-    if( NOT ( MSVC10 OR MSVC11 OR MSVC12 ) )
-        set( ${VAR_ENABLED} 0 )
+        if( NOT ( MSVC10 OR MSVC11 OR MSVC12 ) )
+            set( ${VAR_ENABLED} 0 )
+        endif( )
+
+    elseif( CMAKE_COMPILER_IS_GNUCXX )                  ### CNU
+
+        if( ${CMAKE_CXX_COMPILER_VERSION} VERSION_LESS 4.6 )
+            set( ${VAR_ENABLED} 0 )
+        endif( )
+
+    elseif( CMAKE_CXX_COMPILER_ID STREQUAL "Clang" )    ### Clang
+
+        if( ${CMAKE_CXX_COMPILER_VERSION} VERSION_LESS 3.1 )
+            set( ${VAR_ENABLED} 0 )
+        endif( )
+
     endif( )
-
-elseif( CMAKE_COMPILER_IS_GNUCXX )                  ### CNU
-
-    if( ${CMAKE_CXX_COMPILER_VERSION} VERSION_LESS 4.6 )
-        set( ${VAR_ENABLED} 0 )
-    endif( )
-
-elseif( CMAKE_CXX_COMPILER_ID STREQUAL "Clang" )    ### Clang
-
-    if( ${CMAKE_CXX_COMPILER_VERSION} VERSION_LESS 3.1 )
-        set( ${VAR_ENABLED} 0 )
-    endif( )
-
-endif( )
 
 endmacro( )
