54b3da2c0fac25e642fd9edcf95e0a0517e9a263
newenclave <pikratacid@gmail.com>
2014-09-18 12:30:53 +0400

    ---

--- draft/client/main.cpp
+++ draft/client/main.cpp
@@ -191,7 +191,7 @@ int main( int argc, const char *argv[] ) try
               << "\n";
 
     new_fd->fd_     = fd;
-    new_fd->flags_  = EPOLLIN | EPOLLET | EPOLLPRI | EPOLLOUT;
+    new_fd->flags_  = EPOLLIN | EPOLLET | EPOLLPRI | EPOLLOUT | EPOLLIN;
 
     eventfd_write( add, (eventfd_t)(new_fd) );
     //write( add, &new_fd, sizeof(new_fd) );
