072d2f2d7b1fd7d3da8feb42944f5a07444da579
V.K <kettunen.villu@gmail.com>
2014-09-21 01:02:54 +0400

    channel

--- include/vtrc-common/vtrc-stub-wrapper.h
+++ include/vtrc-common/vtrc-stub-wrapper.h
@@ -53,8 +53,10 @@ namespace vtrc { namespace common {
 
     public:
 
-        stub_wrapper(google::protobuf::RpcChannel *channel)
-            :channel_(channel)
+        stub_wrapper(google::protobuf::RpcChannel *channel,
+                                                    bool own_channel = false)
+            :channel_holder_(own_channel ? channel : NULL)
+            ,channel_(channel)
             ,stub_(channel_)
         { }
 
