57d62b4964f08047649e73537842c6135b96d4b3
V <pikrat.acid@gmail.com>
2016-05-16 14:47:03 +0300

    boost::system -> VTRC_SYSTEM

--- include/vtrc-common/config/vtrc-asio-forward.h
+++ include/vtrc-common/config/vtrc-asio-forward.h
@@ -1,8 +1,7 @@
 #ifndef VTRC_ASIO_FORWARD_H
 #define VTRC_ASIO_FORWARD_H
 
-#define VTRC_ASIO_FORWARD( x ) \
-namespace boost { namespace asio { x } }
+#define VTRC_ASIO_FORWARD( x ) namespace boost { namespace asio { x } }
 
 #define VTRC_ASIO ::boost::asio
 
--- include/vtrc-common/config/vtrc-system-forward.h
+++ include/vtrc-common/config/vtrc-system-forward.h
@@ -1,8 +1,7 @@
 #ifndef VTRC_SYSTEM_FORWARD_H
 #define VTRC_SYSTEM_FORWARD_H
 
-#define VTRC_SYSTEM_FORWARD( x ) \
-namespace boost { namespace system { x } }
+#define VTRC_SYSTEM_FORWARD( x ) namespace boost { namespace system { x } }
 
 #define VTRC_SYSTEM ::boost::system
 
