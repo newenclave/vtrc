a5d4b5f493c027c8e68f7fc10e6ca10346192edd
V <pikrat.acid@gmail.com>
2016-05-16 18:10:48 +0300

    find standalone asio

--- FindAsio.cmake
+++ FindAsio.cmake
@@ -3,19 +3,19 @@ find_path( ASIO_INCLUDE_DIR asio.hpp /usr/include /usr/local/include )
 #find_library(ASIO_LIBRARY NAMES asio PATH /usr/lib /usr/local/lib)
 
 if( ASIO_INCLUDE_DIR )
-   set(ASIO_FOUND TRUE)
+    set( ASIO_FOUND TRUE )
+    set( ASIO_INCLUDE_DIRS ${ASIO_INCLUDE_DIR} )
 endif( ASIO_INCLUDE_DIR )
 
-
 if( ASIO_FOUND )
-   if( NOT ASIO_FIND_QUIETLY )
-      message( STATUS "Found ASIO: ${ASIO_INCLUDE_DIR}" )
-   endif( NOT ASIO_FIND_QUIETLY )
+    if( NOT ASIO_FIND_QUIETLY )
+        message( STATUS "Found ASIO: ${ASIO_INCLUDE_DIR}" )
+    endif( NOT ASIO_FIND_QUIETLY )
 else( ASIO_FOUND )
 
-   if( ASIO_FIND_REQUIRED )
-      message( FATAL_ERROR "Could not find Asio-lib" )
-   endif( ASIO_FIND_REQUIRED )
+    if( ASIO_FIND_REQUIRED )
+        message( FATAL_ERROR "Could not find Asio-lib" )
+    endif( ASIO_FIND_REQUIRED )
 
 endif( ASIO_FOUND )
 
