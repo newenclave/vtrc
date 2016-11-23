0302e7efcbcfc840278b73911b1f9cb71149acef
newenclave <pikrat.acid@gmail.com>
2016-11-23 17:35:38 +0300

    UDP listener BEGIN

---
+++ vtrc-common/vtrc-transport-dgram-impl.h
@@ -0,0 +1,158 @@
+#ifndef VTRC_TRANSPORT_DGRAM_IMPL_H
+#define VTRC_TRANSPORT_DGRAM_IMPL_H
+
+#include <vector>
+
+#include "vtrc-asio.h"
+#include "vtrc-system.h"
+#include "vtrc-memory.h"
+#include "vtrc-function.h"
+#include "vtrc-bind.h"
+#include "vtrc-stdint.h"
+
+namespace vtrc { namespace common {
+
+    namespace basio = VTRC_ASIO;
+    namespace bsys  = VTRC_SYSTEM;
+
+    struct dgram_transport_impl {
+
+        typedef dgram_transport_impl this_type;
+
+        basio::ip::udp::socket      sock_;
+        basio::io_service::strand   dispatcher_;
+        std::vector<std::uint8_t>   data_;
+
+        dgram_transport_impl( basio::io_service &ios, size_t buflen )
+            :sock_(ios)
+            ,dispatcher_(ios)
+            ,data_(buflen ? buflen : 4096)
+        { }
+
+        void close( )
+        {
+            sock_.close( );
+        }
+
+        void write_handler( const bsys::error_code &err, std::size_t len )
+        {
+            on_write( err, len );
+        }
+
+        void read_handler( const bsys::error_code &err, std::size_t len )
+        {
+            const static basio::ip::udp::endpoint empty;
+            on_read( err, empty, &data_[0], len );
+        }
+
+        void read_handler2( const bsys::error_code &err,
+                            std::size_t len,
+                            std::shared_ptr<basio::ip::udp::endpoint> from )
+        {
+            on_read( err, *from, &data_[0], len );
+        }
+
+        void set_buf_size( vtrc::uint16_t len )
+        {
+            data_.resize( len );
+            basio::socket_base::send_buffer_size    snd( len );
+            basio::socket_base::receive_buffer_size rcv( len );
+            bsys::error_code ec;
+            sock_.set_option( snd, ec );
+            sock_.set_option( rcv, ec );
+        }
+
+        const std::vector<std::uint8_t> &get_data( ) const
+        {
+            return data_;
+        }
+
+        void set_buffer_size( vtrc::uint16_t len )
+        {
+            dispatch( std::bind( &this_type::set_buf_size, this, len ) );
+        }
+
+        void open_v4( )
+        {
+            sock_.open( basio::ip::udp::v4( ) );
+        }
+
+        void open_v6( )
+        {
+            sock_.open( basio::ip::udp::v6( ) );
+        }
+
+        basio::io_service &get_io_service( )
+        {
+            return sock_.get_io_service( );
+        }
+
+        void dispatch( vtrc::function<void ()> call )
+        {
+            dispatcher_.dispatch( call );
+        }
+
+        basio::ip::udp::socket &get_socket( )
+        {
+            return sock_;
+        }
+
+        void write( const char *data, size_t len,
+                    basio::socket_base::message_flags flags = 0 )
+        {
+            namespace ph = vtrc::placeholders;
+            sock_.async_send( basio::buffer(data, len),
+                flags,
+                dispatcher_.wrap(
+                   vtrc::bind( &this_type::write_handler, this, ph::_1, ph::_2 )
+                ) );
+        }
+
+        void write_to( const char *data, size_t len,
+                       const basio::ip::udp::endpoint &to,
+                       basio::socket_base::message_flags flags = 0)
+        {
+            namespace ph = vtrc::placeholders;
+            sock_.async_send_to( basio::buffer(data, len), to,
+                flags,
+                dispatcher_.wrap(
+                   vtrc::bind( &this_type::write_handler, this, ph::_1, ph::_2 )
+                ) );
+        }
+
+        void read( basio::socket_base::message_flags flags = 0 )
+        {
+            namespace ph = vtrc::placeholders;
+            sock_.async_receive( basio::buffer(&data_[0], data_.size( )),
+                    flags,
+                    dispatcher_.wrap(
+                        vtrc::bind( &this_type::read_handler, this,
+                                     ph::_1, ph::_2 )
+                    ) );
+        }
+
+        void read_from( const basio::ip::udp::endpoint &from,
+                        basio::socket_base::message_flags flags = 0 )
+        {
+            namespace ph = vtrc::placeholders;
+            vtrc::shared_ptr<basio::ip::udp::endpoint> ep
+                    = vtrc::make_shared<basio::ip::udp::endpoint>(from);
+            sock_.async_receive_from( basio::buffer(&data_[0], data_.size( ) ),
+                            *ep, flags,
+                            dispatcher_.wrap(
+                                vtrc::bind( &this_type::read_handler2, this,
+                                            ph::_1, ph::_2, ep )
+                            ) );
+        }
+
+        virtual void on_write( const bsys::error_code &, std::size_t ) { }
+        virtual void start( ) { }
+        virtual void on_read( const bsys::error_code &,
+                              const basio::ip::udp::endpoint &,
+                              std::uint8_t *, std::size_t ) { }
+
+    };
+
+}}
+
+#endif // VTRC_TRANSPORT_DGRAM_IMPL_H
---
+++ vtrc-server/vtrc-listener-udp.cpp
@@ -0,0 +1,271 @@
+#include "vtrc-asio.h"
+
+#include "vtrc-application.h"
+#include "vtrc-common/vtrc-enviroment.h"
+#include "vtrc-common/vtrc-exception.h"
+#include "vtrc-common/vtrc-connection-list.h"
+
+#include "vtrc-common/vtrc-protocol-accessor-iface.h"
+
+#include "vtrc-rpc-lowlevel.pb.h"
+
+#include "vtrc-connection-impl.h"
+
+#include "vtrc-common/vtrc-closure.h"
+#include "vtrc-common/vtrc-transport-dgram-impl.h"
+
+#include "vtrc-common/vtrc-delayed-call.h"
+
+#include "vtrc-chrono.h"
+
+namespace vtrc { namespace server { namespace listeners {
+
+    namespace {
+
+        static const vtrc::uint64_t timer_interval = 60000000; /// microseconds
+        static const vtrc::uint64_t client_timeout = 60000000; /// microseconds
+
+        struct acceptor_base;
+        typedef vtrc::common::delayed_call delayed_call;
+
+        vtrc::uint64_t ticks_now( )
+        {
+            namespace chrono = vtrc::chrono;
+            using chrono::duration_cast;
+            using microsec = chrono::microseconds;
+
+            chrono::system_clock::time_point n
+                                   = chrono::high_resolution_clock::now( );
+            return duration_cast<microsec>(n.time_since_epoch( )).count( );
+        }
+
+        struct client_info: public common::connection_iface {
+
+            typedef acceptor_base parent_type;
+
+            acceptor_base            *parent_;
+            basio::ip::udp::endpoint  endpoint_;
+            delayed_call              keeper_;
+            vtrc::uint64_t            last_;
+
+            typedef vtrc::shared_ptr<client_info> shared_type;
+            client_info( acceptor_base *parent );
+            void keeper_handler( const bsys::error_code &err );
+
+            void start_keeper( )
+            {
+                namespace ph = vtrc::placeholders;
+                keeper_.call_from_now(
+                    vtrc::bind( &client_info::keeper_handler, this, ph::_1),
+                    delayed_call::microseconds(timer_interval) ); /// settings?
+            }
+
+            std::string name( ) const       { }
+            const std::string &id( )  const { }
+            void init( )                    { }
+            void close( )                   { }
+
+            common::native_handle_type native_handle( ) const;
+
+            void write( const char *data, size_t length ) { }
+            void write( const char *data, size_t length,
+                        const common::system_closure_type &success,
+                        bool success_on_send ) { }
+
+            common::protocol_iface &get_protocol( ) { }
+            const common::protocol_iface &get_protocol( ) const { }
+        };
+
+        typedef std::map< basio::ip::udp::endpoint,
+                          client_info::shared_type
+                        > client_set;
+
+        struct acceptor_base: public common::dgram_transport_impl {
+
+            typedef basio::ip::udp::endpoint endpoint;
+
+            client_set clients_;
+
+            acceptor_base( basio::io_service &ios, size_t len )
+                :common::dgram_transport_impl(ios, len)
+            { }
+
+            size_t size( ) const
+            {
+                return clients_.size( );
+            }
+
+            void drop_client( const endpoint &ep )
+            {
+                clients_.erase( ep );
+            }
+
+            client_info::shared_type get_client( const endpoint &ep )
+            {
+                client_set::iterator f = clients_.find( ep );
+                if( f != clients_.end( ) ) {
+                    return f->second;
+                }
+                return client_info::shared_type( );
+            }
+
+//            virtual void on_write( const bsys::error_code &, std::size_t ) { }
+//            virtual void start( ) { }
+//            virtual void on_read( const bsys::error_code &,
+//                                  const basio::ip::udp::endpoint &,
+//                                  std::uint8_t *, std::size_t ) { }
+
+        };
+
+        struct acceptor_master;
+
+        struct acceptor_slave: public acceptor_base {
+
+            acceptor_master *master_;
+            endpoint ep_;
+
+            typedef vtrc::shared_ptr<acceptor_slave> shared_type;
+
+            acceptor_slave( acceptor_master *master );
+
+            void start( )
+            {
+                ep_.address( ).is_v4( ) ? open_v4( ) : open_v6( );
+                get_socket( ).bind( ep_ );
+                ep_ = get_socket( ).local_endpoint( );
+            }
+
+            static shared_type create( acceptor_master *master );
+        };
+
+        typedef std::list<acceptor_slave::shared_type> slave_list;
+        typedef std::map<size_t, slave_list> slaves_map;
+
+        struct acceptor_master: public acceptor_base {
+
+            endpoint ep_;
+
+            slaves_map slaves_;
+
+            acceptor_master(basio::io_service &ios, size_t len)
+                :acceptor_base(ios, len)
+            { }
+
+            void add_slave(  )
+            {
+                slaves_[0].push_front(acceptor_slave::create( this ));
+            }
+
+            void start( )
+            {
+                ep_.address( ).is_v4( ) ? open_v4( ) : open_v6( );
+            }
+
+            void dec_slave( acceptor_slave *slave )
+            {
+                typedef slave_list::iterator itr;
+                typedef acceptor_slave::shared_type slave_sptr;
+
+                size_t old_size = slave->size( ) + 1;
+                slave_list &lst(slaves_[old_size]);
+
+                for( itr b(lst.begin( )), e(lst.end()); b != e; ++b ) {
+                    if( b->get( ) == slave ) {
+                        slave_sptr slv = *b;
+                        lst.erase( b );
+                        slaves_[old_size - 1].push_front( slv );
+                        break;
+                    }
+                }
+
+                if( lst.empty( ) ) {
+                    slaves_.erase( old_size );
+                }
+            }
+
+            void inc_slave( acceptor_slave *slave )
+            {
+                typedef slave_list::iterator itr;
+                typedef acceptor_slave::shared_type slave_sptr;
+
+                size_t old_size = slave->size( ) - 1;
+                slave_list &lst(slaves_[old_size]);
+
+                for( itr b(lst.begin( )), e(lst.end()); b != e; ++b ) {
+                    if( b->get( ) == slave ) {
+                        slave_sptr slv = *b;
+                        lst.erase( b );
+                        slaves_[old_size + 1].push_front( slv );
+                        break;
+                    }
+                }
+
+                if( lst.empty( ) ) {
+                    slaves_.erase( old_size );
+                }
+            }
+
+            endpoint &get_endpoint( )
+            {
+                return ep_;
+            }
+        };
+
+        ///////////////////////////////// IMPL //////////////////////////////
+
+        ///////// CLIENTS
+        client_info::client_info( acceptor_base *parent )
+            :parent_(parent)
+            ,keeper_(parent_->get_io_service( ))
+            ,last_(ticks_now( ))
+        {
+            start_keeper( );
+        }
+
+        common::native_handle_type client_info::native_handle( ) const
+        {
+            common::native_handle_type nh;
+#ifdef _WIN32
+            nh.value.win_handle =
+                reinterpret_cast<native_handle_type::handle>
+                    ((SOCKET)parent_->get_socket( ).native_handle( ));
+#else
+            nh.value.unix_fd = parent_->get_socket( ).native_handle( );
+#endif
+            return nh;
+        }
+
+        void client_info::keeper_handler( const bsys::error_code &err )
+        {
+            if( !err ) {
+                vtrc::uint64_t now = ticks_now( );
+                if( client_timeout > (now - last_) ) {
+                    parent_->dispatch(
+                        vtrc::bind( &parent_type::drop_client, parent_,
+                                     endpoint_ ) );
+                } else {
+                    start_keeper( );
+                }
+            }
+        }
+
+        ///////// SLAVE
+        acceptor_slave::shared_type acceptor_slave::create( acceptor_master *m )
+        {
+            acceptor_slave::shared_type r
+                    = vtrc::make_shared<acceptor_slave>( m );
+            return r;
+        }
+
+        acceptor_slave::acceptor_slave( acceptor_master *m )
+            :acceptor_base(m->get_io_service( ), m->get_data( ).size( ))
+            ,master_(m)
+            ,ep_(master_->get_endpoint( ).address( ), 0)
+        { }
+
+        /////////////////////////////////////////////////////////////////////
+
+    }
+
+
+}}}
